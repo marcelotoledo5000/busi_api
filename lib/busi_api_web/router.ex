1e6573138f64602a3167e320e071fa6de428e7aa
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-11-12 20:35:58 -0300

    First commit

---
+++ lib/busi_api_web/router.ex
@@ -0,0 +1,37 @@
+defmodule BusiApiWeb.Router do
+  use BusiApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BusiApiWeb do
+    pipe_through :api
+  end
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+  end
+
+  scope "/", BusiApiWeb do
+    pipe_through :browser
+    get "/", DefaultController, :index
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+
+  # if Mix.env() in [:dev, :test] do
+  #   import Phoenix.LiveDashboard.Router
+
+  #   scope "/" do
+  #     pipe_through [:fetch_session, :protect_from_forgery]
+  #     live_dashboard "/dashboard", metrics: BusiApiWeb.Telemetry
+  #   end
+  # end
+end
